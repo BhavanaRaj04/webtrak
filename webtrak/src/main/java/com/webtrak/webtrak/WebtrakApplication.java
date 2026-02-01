3eb3fa4a602ca0744e8862afa62f08d15cf28df6
BhavanaRaj04 <bhavanamanu07@gmail.com>
2026-02-01 12:42:28 +0530

    Initial Spring Boot setup

---
+++ webtrak/src/main/java/com/webtrak/webtrak/WebtrakApplication.java
@@ -0,0 +1,13 @@
+package com.webtrak.webtrak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebtrakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebtrakApplication.class, args);
+	}
+
+}
---
+++ webtrak/src/test/java/com/webtrak/webtrak/WebtrakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webtrak.webtrak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebtrakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
